e421a5519240ac082ce3e71d46f0f9f66859d430
Sumaiya Akter <samm181075@gmail.com>
2026-02-24 04:31:40 +0600

    html er id guli js a const a rakhlam

---
+++ app.js
@@ -0,0 +1,14 @@
+// counter guli k dhore const (namguli) te rekhe dilam jate next a value change korte  pari
+const totalC = document.getElementById("totalC");
+const interViewC = document.getElementById("interViewC");
+const rejectC = document.getElementById("rejectC");
+const availableC = document.getElementById("availableC");
+
+// button guli k dhore const (namguli) te rakhlam jate pore click kore kaaj korate pari
+const allBtn = document.getElementById("allBtn");
+const interViewBtn = document.getElementById("interViewBtn");
+const rejectBtn = document.getElementById("rejectBtn");
+
+// hidden card tak dhore emtystate a rekhe dici , jate pore kaaj korte pari
+const emptyState = document.getElementById("emptyState");
+
